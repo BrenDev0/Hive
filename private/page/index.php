28a981a83d6aa1e722e4833d7c2efb2898f05e68
Brendan Lahey <lahey1991@gmail.com>
2024-11-14 10:34:22 -0500

    WIP: styles and structure

---
+++ private/page/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../private/css/styles.css">
+    <title>Hive: Anonymous Journal</title>
+</head>
+<body>
+    <main>
+        <header>Hive</header>
+        <section></section>
+    </main>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,2 @@
+<?php 
+require '../private/page/index.php';

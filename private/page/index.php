9f015bec1181e314f17ff5f773b219d0380e20ab
Brendan Lahey <lahey1991@gmail.com>
2024-11-17 11:05:38 -0500

    WIP: main styles and layout

--- private/page/index.php
+++ private/page/index.php
@@ -4,22 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../private/css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kavoon&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
     <title>Hive: Anonymous Journal</title>
 </head>
 <body>
     <main>
-        <header>Hive</header>
+        <header>
+            <h1>Hive</h1>
+            <img src="../private/assets/Group 3.png" alt="" />
+        </header>
+        <?php include '../private/templates/nav.php';?>
         <div id="main-con">
             <section id="toolbar">
                 <input type="text">
                 <div id="tags"></div>
             </section>
-            <div id="content-con">
-                <?php include '../private/templates/nav.php';?>
-                <section id='content'>
-                    <!-- render content here -->
-                </section>
-            </div>
+            <section id='content'>
+                <!-- render content here -->
+            </section>
         </div>
     </main>
 </body>
--- private/templates/nav.php
+++ private/templates/nav.php
@@ -1,6 +1,12 @@
 <nav id="nav-bar">
     <ul>
-        <li><a href="post.php">Post</a></li>
-        <li><a href="state.php">State of the Hive</a></li>
+        <li>
+            <a class="nav-icon" href="post.php"><i class="fas fa-edit"></i></a>
+            <a class="nav-text" href="post.php">Post</a>
+        </li>
+        <li>
+            <a class="nav-icon" href="post.php"><i class="fas fa-globe"></i></a>
+            <a class="nav-text" href="post.php">State of the Hive</a>
+        </li>
     </ul>
 </nav>
